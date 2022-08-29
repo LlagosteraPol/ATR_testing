ef85da6810c4c72563a30d5d3187b62d41f643cd
Pol Llagostera Blasco <plb92@hotmail.com>
2022-08-29 11:30:26 +0200

    - Tested that the ModuleCake effectively works with simple networks but declared as a networkx MultiGraph object.

--- Tests/unit_tests.py
+++ Tests/unit_tests.py
@@ -89,7 +89,7 @@ class TestModuleCycle(unittest.TestCase):
 class TestModuleCake(unittest.TestCase):
 
     def setUp(self):
-        self.g = nx.Graph([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 1), (1, 5), (2, 6), (3, 7), (4, 8)])
+        self.g = nx.MultiGraph([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 1), (1, 5), (2, 6), (3, 7), (4, 8)])
         #self.g  = nx.Graph([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 1), (1, 5), (2, 6), (3, 7)])
         self.cakemodule = relmodules.ModuleCake(self.g)
 
